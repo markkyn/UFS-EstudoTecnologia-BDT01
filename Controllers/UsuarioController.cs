26212614bbae37752d8e178bfe19a82d0645d457
Marcos Gabriel da Silva Santos <marcos.santos@lais.huol.ufrn.br>
2023-08-20 17:23:29 -0300

    zero: first commit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+using SistemaOcorrencias.Models;
+
+namespace SistemaOcorrencias.Controllers
+{
+    namespace MinhaApi.Controllers
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class UsuarioController : ControllerBase
+        {
+            // Não estou utilizando o EntityFramework para acesso ao banco de dado
+            private static List<Usuario> _usuarios = new List<Usuario>();
+
+            [HttpGet]
+            public IEnumerable<Usuario> GetUsuarios()
+            {
+                // Retorna todos os Usuarios
+
+                return _usuarios;
+            }
+
+            [HttpPost]
+            public IActionResult CriarUsuario(Usuario usuario)
+            {
+                // Verificação de Usuario já criado
+                var user = _usuarios.Find(u => u.cpf == usuario.cpf);
+                if (user != null)
+                {
+                    return NotFound();
+                }
+
+                return CreatedAtAction(nameof(GetUsuario), new { cpf = usuario.cpf }, usuario);
+            }
+
+            [HttpGet("{cpf}")]
+            public ActionResult<Usuario> GetUsuario(UInt64 cpf)
+            {
+                // Retorna Usuario específico
+                var usuario = _usuarios.Find(u => u.cpf == cpf);
+                if (usuario == null)
+                {
+                    return NotFound();
+                }
+                return usuario;
+            }
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SistemaOcorrencias.Models
+{
+
+    public class Usuario
+    {
+        [Key]
+        [Required]
+        public UInt64  cpf { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string nome { get; set; }
+
+        [Required]
+        public DateTime data_nascimento { get; set; }
+    }
+}
